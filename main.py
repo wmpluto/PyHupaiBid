15e85218071dd14fb83a370b5ed60659e9d912e5
Damien Wang <damien.wang@microchip.com>
2020-07-03 15:21:30 +0800

    add ocr

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import time
 import keyboard
 import pyautogui
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 
 RELEASE = False
-
+PRICE_AREA = (732, 538, 141, 40)
 print("Good Luck!")
+
+pytesseract.pytesseract.tesseract_cmd = r'D:/tmp/hupai/tesseract-v5/tesseract.exe'
 
 while True:
     if (RELEASE):
@@ -14,9 +21,13 @@ while True:
     else:
         print("Press enter when it's 11:29:10: ")
         keyboard.wait('enter')
+        start_time = time.time()
         print("...")
-        ## Wait for 47 seconds
-        time.sleep(47-0.2)
+        ## time.sleep(47-0.2)
+        while(time.time() < (start_time + 47 - 0.2)):
+            price_img = pyautogui.screenshot(region = PRICE_AREA)
+            r = pytesseract.image_to_string(price_img, config='outputbase digits')
+            print(r) 
 
     pyautogui.click()
     print(time.asctime(time.localtime(time.time())))
