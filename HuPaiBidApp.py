8309f37ce2779588ce24e0953ad916e95d7ffd41
Damien Wang <damien.wang@microchip.com>
2020-07-05 19:02:31 +0800

    Update UI

--- HuPaiBidApp.py
+++ HuPaiBidApp.py
@@ -20,7 +20,7 @@ class HuPaiBidApp(HuPaiBidGui):
     rise_prise_button_pos = (0, 0)
     submit_button_pos = (0, 0)
     verification_code_input_pos = (0, 0)
-    confirm_after_bid_button_pos = (0, 0)  
+    confirm_after_bid_button_pos = (0, 0)
 
     def __init__(self):
         HuPaiBidGui.__init__(self)
@@ -28,23 +28,27 @@ class HuPaiBidApp(HuPaiBidGui):
         self.debug = False
         self.event = threading.Event()
         #_thread.start_new_thread( thread_ocr, ("price", ))
-        self.update_time_display_t = threading.Thread(target=self.update_time_display, args=())
+        self.update_time_display_t = threading.Thread(
+            target=self.update_time_display, args=())
         self.update_time_display_t.setDaemon(True)
         self.update_time_display_t.start()
-        self.update_price_display_t = threading.Thread(target=self.update_price_display, args=())
+        self.update_price_display_t = threading.Thread(
+            target=self.update_price_display, args=())
         self.update_price_display_t.setDaemon(True)
         self.update_price_display_t.start()
-        self.local_time = time.time()        
+        self.local_time = time.time()
         self.mainloop()
 
     def update_time_display(self):
         while True:
             try:
                 if self.debug:
-                    self.current_time = time.time() - self.debug_start_time + DBG_TIMESTAMP + float(self.time_drift_input.get())
+                    self.current_time = time.time() - self.debug_start_time + DBG_TIMESTAMP + \
+                        float(self.time_drift_entry.get())
                 else:
-                    self.current_time = time.time() + float(self.time_drift_input.get())
-                self.current_time_text.set(time.strftime("%H:%M:%S", time.localtime(self.current_time)))
+                    self.current_time = time.time() + float(self.time_drift_entry.get())
+                self.current_time_text.set(time.strftime(
+                    "%H:%M:%S", time.localtime(self.current_time)))
                 time.sleep(0.01)
             except:
                 pass
@@ -53,42 +57,47 @@ class HuPaiBidApp(HuPaiBidGui):
     def update_price_display(self):
         self.event.wait()
 
-        RMB_AREA = auto.locateOnScreen('./img/rmb.png', region = LEFT_AREA, confidence=0.9, grayscale=True)
-        PRICE_AREA = (RMB_AREA.left  + RMB_AREA.width, RMB_AREA.top, 140 , RMB_AREA.height)
+        RMB_AREA = auto.locateOnScreen(
+            './img/rmb.png', region=LEFT_AREA, confidence=0.9, grayscale=True)
+        PRICE_AREA = (RMB_AREA.left + RMB_AREA.width,
+                      RMB_AREA.top, 140, RMB_AREA.height)
 
         pytesseract.pytesseract.tesseract_cmd = r'./tesseract/tesseract.exe'
 
         while True:
             try:
-                price_img = auto.screenshot(region = PRICE_AREA)
+                price_img = auto.screenshot(region=PRICE_AREA)
 
-                new_img = Image.new(mode='RGB',size=(140*2, 40*2),color=(255,255,255))
+                new_img = Image.new(mode='RGB', size=(
+                    140*2, 40*2), color=(255, 255, 255))
                 new_img.paste(price_img, (int(140/2), int(40/2)))
                 new_img.save("tmp.png")
                 r = pytesseract.image_to_string(new_img)
-                print(int("".join(list(filter(str.isdigit,r)))))
-                self.current_price_text.set(int("".join(list(filter(str.isdigit,r)))))
+                print(int("".join(list(filter(str.isdigit, r)))))
+                self.current_price_text.set(
+                    int("".join(list(filter(str.isdigit, r)))))
             except:
-                print("OCR Wrong!")     
-                self.current_price_text.set("00000")       
-                 
+                print("OCR Wrong!")
+                self.current_price_text.set("00000")
+
     def debug_start(self):
         self.debug = True
         self.debug_start_time = time.time()
         print(DBG_TIMESTAMP)
 
-        print(self.time_drift_input.get())
-        print(self.first_bid_time_input.get())
-        print(self.first_bid_price_input.get())
-        print(self.second_bid_time_input.get())
-        print(self.second_bid_price_input.get())
+        print(self.time_drift_entry.get())
+        print(self.first_bid_time_entry.get())
+        print(self.first_bid_price_entry.get())
+        print(self.second_bid_time_entry.get())
+        print(self.second_bid_price_entry.get())
         self.clear_log_display()
 
     def release_start(self):
         self.debug = False
 
     def get_pos(self, img):
-        area = auto.locateOnScreen(img, region=(900, 300, 500, 400), confidence=0.9) 
+        area = auto.locateOnScreen(img, region=(
+            900, 300, 500, 400), confidence=0.9)
         try:
             center = auto.center(area)
         except:
@@ -108,10 +117,11 @@ class HuPaiBidApp(HuPaiBidGui):
         custom_price_input_pos = self.get_pos(CUSTOM_PRICE_INPUT_PNG)
         if custom_price_input_pos:
             auto.click(custom_price_input_pos)
-            auto.typewrite(message='600',interval=0.2)
+            auto.typewrite(message='600', interval=0.2)
             auto.moveTo(custom_price_input_pos)
         else:
-            custom_price_input_pos = (rise_prise_button_pos[0] - 100, rise_prise_button_pos[1])
+            custom_price_input_pos = (
+                rise_prise_button_pos[0] - 100, rise_prise_button_pos[1])
             auto.moveTo(custom_price_input_pos)
         print(custom_price_input_pos)
         time.sleep(CAL_DELAY)
@@ -126,19 +136,19 @@ class HuPaiBidApp(HuPaiBidGui):
         bid_button_pos = self.get_pos(BID_BUTTON_PNG)
         auto.moveTo(bid_button_pos)
         print(bid_button_pos)
-        time.sleep(CAL_DELAY) 
+        time.sleep(CAL_DELAY)
 
         self.event.set()
 
     def update_log_display(self, log):
-        self.log_display.insert(tk.INSERT, log + '\n');
-        self.log_display.see(tk.END);  
+        self.log_display_text.insert(tk.INSERT, log + '\n')
+        self.log_display_text.see(tk.END)
 
     def clear_log_display(self):
-        self.log_display.delete(0.0, tk.END)
+        self.log_display_text.delete(0.0, tk.END)
 
     def about(self):
         tk.messagebox.showinfo('关于', 'Python版拍牌小助手\nwmpluto@gmail.com')
-    
+
 
 a = HuPaiBidApp()
--- HuPaiBidGui.py
+++ HuPaiBidGui.py
@@ -3,13 +3,14 @@ from tkinter import ttk
 import tkinter as tk
 import tkinter.scrolledtext as tkst
 
+
 class HuPaiBidGui:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("拍牌助手")
-        self.root.wm_attributes('-topmost',1)
+        self.root.wm_attributes('-topmost', 1)
         self.root.geometry('217x260')
-        self.root.resizable(0, 0)  
+        self.root.resizable(0, 0)
         self.create_menu()
         self.creat_widgets()
 
@@ -18,76 +19,107 @@ class HuPaiBidGui:
 
     def create_menu(self):
         menu_bar = tk.Menu(self.root)
-        menu_bar.add_command(label = "关于", command=self.about)
+        menu_bar.add_command(label="关于", command=self.about)
         self.root['menu'] = menu_bar
 
     def creat_widgets(self):
         self.frame_one = tk.Frame(self.root)
-        self.debug_start_btn = ttk.Button(self.frame_one, text="模拟", width=5, command = self.debug_start)  
-        self.debug_start_btn.pack(side = tk.LEFT, padx=1, pady=1)
-        self.release_start_btn = ttk.Button(self.frame_one, text="实战", width=5, command = self.release_start)  
-        self.release_start_btn.pack(side = tk.LEFT, padx=1, pady=1)
+
+        self.debug_start_btn = ttk.Button(
+            self.frame_one, text="模拟", width=5, command=self.debug_start)
+        self.debug_start_btn.pack(side=tk.LEFT, padx=1, pady=1)
+        self.release_start_btn = ttk.Button(
+            self.frame_one, text="实战", width=5, command=self.release_start)
+        self.release_start_btn.pack(side=tk.LEFT, padx=1, pady=1)
         self.time_drift_lbl = ttk.Label(self.frame_one, text="时间微调:")
-        self.time_drift_lbl.pack(side = tk.LEFT, padx=1, pady=1)    
-        self.time_drift_input = ttk.Entry(self.frame_one, width=5, justify=tk.RIGHT)
-        self.time_drift_input.pack(side = tk.LEFT, padx=1, pady=1) 
-        self.time_drift_input.insert(tk.END, '0')    
+        self.time_drift_lbl.pack(side=tk.LEFT, padx=1, pady=1)
+        self.time_drift_entry = ttk.Entry(
+            self.frame_one, width=5, justify=tk.RIGHT)
+        self.time_drift_entry.pack(side=tk.LEFT, padx=1, pady=1)
+        self.time_drift_entry.insert(tk.END, '0')
         self.drift_unit_lbl = ttk.Label(self.frame_one, text="秒")
-        self.drift_unit_lbl.pack(side = tk.LEFT, padx=1, pady=1)           
-        self.frame_one.pack(side = tk.TOP, expand = tk.YES, fill=tk.X) 
+        self.drift_unit_lbl.pack(side=tk.LEFT, padx=1, pady=1)
+
+        self.frame_one.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
 
         self.frame_two = tk.Frame(self.root)
+
         self.current_time_lbl = ttk.Label(self.frame_two, text="当前时间:")
-        self.current_time_lbl.pack(side = tk.LEFT, padx=1, pady=1)
+        self.current_time_lbl.pack(side=tk.LEFT, padx=1, pady=1)
         self.current_time_text = tk.StringVar()
-        self.current_time_display_lbl = ttk.Label(self.frame_two, text="11:29:00", relief="groove", textvariable=self.current_time_text)
-        self.current_time_display_lbl.pack(side = tk.LEFT, padx=1, pady=1)
+        self.current_time_display_lbl = ttk.Label(
+            self.frame_two, relief="groove", textvariable=self.current_time_text)
+        self.current_time_text.set("11:29:00")
+        self.current_time_display_lbl.pack(side=tk.LEFT, padx=1, pady=1)
         self.current_price_lbl = ttk.Label(self.frame_two, text="当前价格:")
-        self.current_price_lbl.pack(side = tk.LEFT, padx=1, pady=1)
+        self.current_price_lbl.pack(side=tk.LEFT, padx=1, pady=1)
         self.current_price_text = tk.StringVar()
-        self.current_price_display_lbl = ttk.Label(self.frame_two, text="86000", relief="groove", textvariable=self.current_price_text)
-        self.current_price_text.set("86000")
-        self.current_price_display_lbl.pack(side = tk.LEFT, padx=1, pady=1)
-        self.frame_two.pack(side = tk.TOP)
+        self.current_price_display_lbl = ttk.Label(
+            self.frame_two, relief="groove", textvariable=self.current_price_text)
+        self.current_price_text.set("00000")
+        self.current_price_display_lbl.pack(side=tk.LEFT, padx=1, pady=1)
+
+        self.frame_two.pack(side=tk.TOP)
 
         self.frame_three = tk.Frame(self.root)
-        self.screnn_coordinate_calibration_btn = ttk.Button(self.frame_three, text="屏幕校准", command = self.screnn_coordinate_calibration)
-        self.screnn_coordinate_calibration_btn.pack(side = tk.LEFT, expand = tk.YES, fill=tk.X, padx=1, pady=1)
-        self.frame_three.pack(side = tk.TOP, expand = tk.YES, fill=tk.X) 
+
+        self.screnn_coordinate_calibration_btn = ttk.Button(
+            self.frame_three, text="屏幕校准", command=self.screnn_coordinate_calibration)
+        self.screnn_coordinate_calibration_btn.pack(
+            side=tk.LEFT, expand=tk.YES, fill=tk.X, padx=1, pady=1)
+
+        self.frame_three.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
 
         self.frame_four = tk.Frame(self.root)
-        self.frame_four_time_icon_lbl = ttk.Label(self.frame_four, text="预定时间") 
-        self.frame_four_time_icon_lbl.grid(row=0, column=1, columnspan=2, padx=1, pady=1)
-        self.frame_four_price_icon_lbl = ttk.Label(self.frame_four, text="自定义加价") 
-        self.frame_four_price_icon_lbl.grid(row=0, column=3, padx=1, pady=1)        
 
-        self.first_bid_lbl = ttk.Label(self.frame_four, text="第一次出价: ") 
+        self.frame_four_time_icon_lbl = ttk.Label(self.frame_four, text="预定时间")
+        self.frame_four_time_icon_lbl.grid(
+            row=0, column=1, columnspan=2, padx=1, pady=1)
+        self.frame_four_price_icon_lbl = ttk.Label(
+            self.frame_four, text="自定义加价")
+        self.frame_four_price_icon_lbl.grid(row=0, column=3, padx=1, pady=1)
+
+        self.first_bid_lbl = ttk.Label(self.frame_four, text="第一次出价: ")
         self.first_bid_lbl.grid(row=1, column=0, padx=1, pady=1)
-        self.first_bid_time_prefix_lbl = ttk.Label(self.frame_four, text="11:29:") 
+        self.first_bid_time_prefix_lbl = ttk.Label(
+            self.frame_four, text="11:29:")
         self.first_bid_time_prefix_lbl.grid(row=1, column=1, padx=1, pady=1)
-        self.first_bid_time_input = ttk.Entry(self.frame_four, width=4, justify=tk.LEFT)
-        self.first_bid_time_input.grid(row=1, column=2, padx=1, pady=1)
-        self.first_bid_time_input.insert(tk.END, '30')
-        self.first_bid_price_input = ttk.Entry(self.frame_four, width=5, justify=tk.RIGHT)
-        self.first_bid_price_input.grid(row=1, column=3, padx=1, pady=1)
-        self.first_bid_price_input.insert(tk.END, '300')
+        self.first_bid_time_entry_text = tk.StringVar()
+        self.first_bid_time_entry = ttk.Entry(
+            self.frame_four, width=4, justify=tk.LEFT, textvariable=self.first_bid_time_entry_text)
+        self.first_bid_time_entry_text.set("00")
+        self.first_bid_time_entry.grid(row=1, column=2, padx=1, pady=1)
+        self.first_bid_price_entry_text = tk.StringVar()
+        self.first_bid_price_entry_text.set("300")
+        self.first_bid_price_entry = ttk.Entry(
+            self.frame_four, width=5, justify=tk.RIGHT, state='readonly', textvariable=self.first_bid_price_entry_text)
+        self.first_bid_price_entry.grid(row=1, column=3, padx=1, pady=1)
 
-        self.second_bid_lbl = ttk.Label(self.frame_four, text="第二次出价: ") 
+        self.second_bid_lbl = ttk.Label(self.frame_four, text="第二次出价: ")
         self.second_bid_lbl.grid(row=2, column=0, padx=1, pady=1)
-        self.second_bid_time_prefix_lbl = ttk.Label(self.frame_four, text="11:29:") 
-        self.second_bid_time_prefix_lbl.grid(row=2, column=1, padx=1, pady=1)        
-        self.second_bid_time_input = ttk.Entry(self.frame_four, width=4, justify=tk.LEFT)
-        self.second_bid_time_input.grid(row=2, column=2, padx=1, pady=1)
-        self.second_bid_time_input.insert(tk.END, '48')
-        self.second_bid_price_input = ttk.Entry(self.frame_four, width=5, justify=tk.RIGHT)
-        self.second_bid_price_input.grid(row=2, column=3, padx=1, pady=1)
-        self.second_bid_price_input.insert(tk.END, '600')  
-        self.frame_four.pack(side = tk.TOP)
+        self.second_bid_time_prefix_lbl = ttk.Label(
+            self.frame_four, text="11:29:")
+        self.second_bid_time_prefix_lbl.grid(row=2, column=1, padx=1, pady=1)
+        self.second_bid_time_entry_text = tk.StringVar()
+        self.second_bid_time_entry = ttk.Entry(
+            self.frame_four, width=4, justify=tk.LEFT, textvariable=self.second_bid_time_entry_text)
+        self.second_bid_time_entry_text.set("00")
+        self.second_bid_time_entry.grid(row=2, column=2, padx=1, pady=1)
+        self.second_bid_price_entry_text = tk.StringVar()
+        self.second_bid_price_entry_text.set("600")
+        self.second_bid_price_entry = ttk.Entry(
+            self.frame_four, width=5, justify=tk.RIGHT, textvariable=self.second_bid_price_entry_text)
+        self.second_bid_price_entry.grid(row=2, column=3, padx=1, pady=1)
+
+        self.frame_four.pack(side=tk.TOP)
 
         self.frame_five = tk.Frame(self.root)
-        self.log_display = tkst.ScrolledText(self.frame_five, wrap=tk.WORD)
-        self.log_display.pack()  
-        self.frame_five.pack(side = tk.TOP)      
+
+        self.log_display_text = tkst.ScrolledText(
+            self.frame_five, wrap=tk.WORD)
+        self.log_display_text.pack()
+
+        self.frame_five.pack(side=tk.TOP)
 
     def screnn_coordinate_calibration(self):
         pass
@@ -100,5 +132,3 @@ class HuPaiBidGui:
 
     def about(self):
         pass
-
-    
